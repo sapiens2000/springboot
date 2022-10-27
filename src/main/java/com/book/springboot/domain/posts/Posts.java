efcc5af7f36718ca420f8b895b219fd4548c2230
ByeongGyu Jeon <ntnegm@gmail.com>
2022-10-28 01:24:13 +0900

    in chapter 3

---
+++ src/main/java/com/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,14 @@
+package com.book.springboot.domain.posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+public class Posts {
+}
