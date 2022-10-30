dde1e23d7ffce17a823a59fe23a04eba52c095e9
ByeongGyu Jeon <ntnegm@gmail.com>
2022-10-30 21:53:55 +0900

    chapter 3

--- src/main/java/com/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/book/springboot/domain/posts/Posts.java
@@ -1,4 +1,5 @@
 package com.book.springboot.domain.posts;
+import com.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,5 +11,31 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Getter
-public class Posts {
+@NoArgsConstructor
+@Entity
+public class Posts extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
 }
